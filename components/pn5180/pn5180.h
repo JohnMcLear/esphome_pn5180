5c4e95c5ce581f63085937dc4522d8f374584f13
John McLear <john@mclear.co.uk>
2026-02-12 14:08:26 +0000

    lb

--- components/pn5180/pn5180.h
+++ components/pn5180/pn5180.h
@@ -1,12 +1,20 @@
 #pragma once
+
 #include "esphome.h"
+#include "PN5180.h"
+#include "PN5180ISO15693.h"
 
 namespace esphome {
 namespace pn5180 {
 
 class PN5180Component : public PollingComponent, public text_sensor::TextSensor {
  public:
-  PN5180Component(GPIOPin *cs, GPIOPin *busy, GPIOPin *rst, uint32_t update_interval);
+  PN5180Component(
+      GPIOPin *cs,
+      GPIOPin *busy,
+      GPIOPin *rst,
+      uint32_t update_interval
+  );
 
   void setup() override;
   void update() override;
@@ -15,6 +23,9 @@ class PN5180Component : public PollingComponent, public text_sensor::TextSensor 
   GPIOPin *cs_;
   GPIOPin *busy_;
   GPIOPin *rst_;
+  uint32_t update_interval_;
+
+  PN5180ISO15693 pn5180_;
 };
 
 }  // namespace pn5180
