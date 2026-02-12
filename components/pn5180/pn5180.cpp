b102ee6b83765b7a01eaed263f1b493a256444e1
John McLear <john@mclear.co.uk>
2026-02-12 13:25:51 +0000

    shuffle

---
+++ components/pn5180/pn5180.cpp
@@ -0,0 +1,38 @@
+#include "pn5180.h"
+
+void PN5180Component::setup() {
+  cs_pin_->setup();
+  busy_pin_->setup();
+  rst_pin_->setup();
+
+  pn5180_.begin();
+  pn5180_.reset();
+  pn5180_.setupRF();
+}
+
+void PN5180Component::update() {
+  static bool default_state = false;
+  uint8_t uid[8];
+  ISO15693ErrorCode rc = pn5180_.getInventory(uid);
+  if (ISO15693_EC_OK == rc) {
+    String uid_str = "";
+    for (uint8_t i = 0; i < sizeof(uid); i++) {
+      uid_str += String(uid[i] < 0x10 ? " 0" : " ");
+      uid_str += String(uid[i], HEX);
+    }
+    uid_str.trim();
+
+    ESP_LOGI("pn5180", "Read UID: %s", uid_str.c_str());
+    publish_state(uid_str.c_str());
+    default_state = false;
+
+    pn5180_.reset();
+    pn5180_.setupRF();
+  } else {
+    if (!default_state) {
+      ESP_LOGI("pn5180", "No NFC detected");
+      publish_state("No NFC detected");
+    }
+    default_state = true;
+  }
+}
--- components/pn5180/pn5180.h
+++ components/pn5180/pn5180.h
@@ -6,62 +6,17 @@
 
 using namespace esphome;
 
-class PN5180Component :  public PollingComponent, public text_sensor::TextSensor {
+class PN5180Component : public PollingComponent, public text_sensor::TextSensor {
  public:
-  // Constructor with pin assignments and update interval
-  PN5180Component(uint8_t pin_ss, uint8_t pin_busy, uint8_t pin_rst, uint32_t update_interval)
-      :   PollingComponent(update_interval), pn5180_(pin_ss, pin_busy, pin_rst) {}
+  PN5180Component(InternalGPIOPin *cs_pin, InternalGPIOPin *busy_pin, InternalGPIOPin *rst_pin, uint32_t update_interval)
+      : PollingComponent(update_interval), cs_pin_(cs_pin), busy_pin_(busy_pin), rst_pin_(rst_pin) {}
 
-
-  // Setup function, called once during initialization
-  void setup() override {
-     // Initialize the PN5180 instance
-     pn5180_.begin();  
-
-     // Reset and set up the RF settings for the PN5180
-     pn5180_.reset();
-     pn5180_.setupRF();
-  }
-
-
-  // Update function, called periodically according to the update interval
-  void update() override {
-    static bool default_state = false;
-    uint8_t uid[8];
-    // Attempt to read the UID of an ISO15693 tag
-    ISO15693ErrorCode rc =  pn5180_.getInventory(uid);
-    if (ISO15693_EC_OK == rc) {
-      // Convert the UID to a string
-      String uid_str = "";
-      for (uint8_t i = 0; i < sizeof(uid); i++) {
-        uid_str += String(uid[i] < 0x10 ? " 0" : " ");
-        uid_str += String(uid[i], HEX);
-      }
-      uid_str.trim();
-
-      // Print the UID to the log
-      ESP_LOGI("pn5180", "Read UID: %s", uid_str.c_str());
-
-      // Update the text sensor with the UID value
-      publish_state(uid_str.c_str());
-      default_state = false;
-
-      // Reset and set up the RF settings for the PN5180 for the next read
-      pn5180_.reset();
-      pn5180_.setupRF();
-    } else {
-      // If reading the UID fails, log an error and update the text sensor to show no tag detected
-      if (!default_state)
-      {
-        ESP_LOGI("pn5180", "No NFC detected");
-        publish_state("No NFC detected");
-      }
-      default_state = true;
-    }
-  }
-
+  void setup() override;
+  void update() override;
 
  protected:
-  // The PN5180 instance used for reading ISO15693 tags
+  InternalGPIOPin *cs_pin_;
+  InternalGPIOPin *busy_pin_;
+  InternalGPIOPin *rst_pin_;
   PN5180ISO15693 pn5180_;
 };
