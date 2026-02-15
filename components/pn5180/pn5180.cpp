221697be4eaa00099f259c1f2352bc93196416c9
John McLear <john@mclear.co.uk>
2026-02-15 08:16:32 +0000

    should compile

--- components/pn5180/pn5180.cpp
+++ components/pn5180/pn5180.cpp
@@ -1,51 +1,66 @@
 #include "pn5180.h"
+#include "esphome/core/log.h"
 
 namespace esphome {
 namespace pn5180 {
 
+static const char *TAG = "pn5180";
+
 PN5180Component::PN5180Component(
-    GPIOPin *cs,
-    GPIOPin *busy,
-    GPIOPin *rst,
-    uint32_t update_interval
-)
-    : cs_(cs),
+    InternalGPIOPin *cs,
+    InternalGPIOPin *busy,
+    InternalGPIOPin *rst,
+    uint32_t update_interval)
+    : PollingComponent(update_interval),
+      cs_(cs),
       busy_(busy),
       rst_(rst),
-      update_interval_(update_interval) {}
+      pn5180_(nullptr) {}
 
 void PN5180Component::setup() {
-  this->pn5180_ = PN5180ISO15693(
+  ESP_LOGI(TAG, "Initializing PN5180");
+
+  this->pn5180_ = new PN5180ISO15693(
       this->cs_->get_pin(),
       this->busy_->get_pin(),
       this->rst_->get_pin()
   );
-  this->pn5180_.begin();
-  this->pn5180_.reset();
-  this->pn5180_.setupRF();
+
+  this->pn5180_->begin();
+  this->pn5180_->reset();
+  this->pn5180_->setupRF();
 }
 
 void PN5180Component::update() {
-  static bool default_state = false;
+  if (this->pn5180_ == nullptr)
+    return;
+
+  static bool no_tag_reported = false;
   uint8_t uid[8];
 
-  ISO15693ErrorCode rc = this->pn5180_.getInventory(uid);
-  if (ISO15693_EC_OK == rc) {
-    String uid_str = "";
+  ISO15693ErrorCode rc = this->pn5180_->getInventory(uid);
+
+  if (rc == ISO15693_EC_OK) {
+    char buffer[32];
+    buffer[0] = '\0';
+
     for (uint8_t i = 0; i < sizeof(uid); i++) {
-      uid_str += String(uid[i] < 0x10 ? " 0" : " ");
-      uid_str += String(uid[i], HEX);
+      char part[4];
+      sprintf(part, "%02X", uid[i]);
+      strcat(buffer, part);
+      if (i < sizeof(uid) - 1)
+        strcat(buffer, " ");
     }
-    uid_str.trim();
-    ESP_LOGI("pn5180", "Read UID: %s", uid_str.c_str());
-    this->publish_state(uid_str.c_str());
-    default_state = false;
+
+    ESP_LOGI(TAG, "Tag detected: %s", buffer);
+    this->publish_state(buffer);
+    no_tag_reported = false;
   } else {
-    if (!default_state) {
-      ESP_LOGI("pn5180", "No NFC detected");
+    if (!no_tag_reported) {
+      ESP_LOGD(TAG, "No tag");
       this->publish_state("No NFC detected");
+      no_tag_reported = true;
     }
-    default_state = true;
   }
 }
 
--- components/pn5180/pn5180.h
+++ components/pn5180/pn5180.h
@@ -1,53 +1,32 @@
-#include "pn5180.h"
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+#include "esphome/components/text_sensor/text_sensor.h"
+#include <PN5180ISO15693.h>
 
 namespace esphome {
 namespace pn5180 {
 
-PN5180Component::PN5180Component(
-    GPIOPin *cs,
-    GPIOPin *busy,
-    GPIOPin *rst,
-    uint32_t update_interval
-)
-  : cs_(cs),
-    busy_(busy),
-    rst_(rst),
-    update_interval_(update_interval) {}
+class PN5180Component : public PollingComponent,
+                        public text_sensor::TextSensor {
+ public:
+  PN5180Component(
+      InternalGPIOPin *cs,
+      InternalGPIOPin *busy,
+      InternalGPIOPin *rst,
+      uint32_t update_interval);
 
-void PN5180Component::setup() {
-  this->pn5180_ = PN5180ISO15693(
-      this->cs_->get_pin(),
-      this->busy_->get_pin(),
-      this->rst_->get_pin()
-  );
-  this->pn5180_.begin();
-  this->pn5180_.reset();
-  this->pn5180_.setupRF();
-}
+  void setup() override;
+  void update() override;
 
-void PN5180Component::update() {
-  static bool default_state = false;
-  uint8_t uid[8];
+ protected:
+  InternalGPIOPin *cs_;
+  InternalGPIOPin *busy_;
+  InternalGPIOPin *rst_;
 
-  ISO15693ErrorCode rc = this->pn5180_.getInventory(uid);
-  if (ISO15693_EC_OK == rc) {
-    String uid_str = "";
-    for (uint8_t i = 0; i < sizeof(uid); i++) {
-      uid_str += String(uid[i] < 0x10 ? " 0" : " ");
-      uid_str += String(uid[i], HEX);
-    }
-    uid_str.trim();
-    ESP_LOGI("pn5180", "Read UID: %s", uid_str.c_str());
-    this->publish_state(uid_str.c_str());
-    default_state = false;
-  } else {
-    if (!default_state) {
-      ESP_LOGI("pn5180", "No NFC detected");
-      this->publish_state("No NFC detected");
-    }
-    default_state = true;
-  }
-}
+  PN5180ISO15693 *pn5180_;
+};
 
 }  // namespace pn5180
 }  // namespace esphome
